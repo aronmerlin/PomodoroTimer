24cae00753fcd0acc3d0ede5011b6d8ecf578036
Aron-Merlin Schlegel <aronmerlin.schlegel@student.hs-rm.de>
2024-09-06 18:14:25 +0200

    Controller Konstruktor verbessert

--- app/src/main/java/org/example/Controller/PomodoroController.java
+++ app/src/main/java/org/example/Controller/PomodoroController.java
@@ -4,11 +4,12 @@ import org.example.Model.PomodoroModel;
 import org.example.View.PomodoroView;
 
 public class PomodoroController {
-    PomodoroModel pomodoroModel = new PomodoroModel();
-    PomodoroView pomodoroView = new PomodoroView();
+    PomodoroModel pomodoroModel;
+    PomodoroView pomodoroView;
 
     public PomodoroController(PomodoroModel pomodoroModel, PomodoroView pomodoroView) {
-       
+       this.pomodoroModel = pomodoroModel;
+       this.pomodoroView = pomodoroView;
     }
     
 }
