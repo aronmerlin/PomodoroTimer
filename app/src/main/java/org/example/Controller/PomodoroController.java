6b923cf7eef1509c0de458f4d969919d91bd40f4
Aron-Merlin Schlegel <aronmerlin.schlegel@student.hs-rm.de>
2024-09-06 11:35:27 +0200

    Basic classes implemented

---
+++ app/src/main/java/org/example/Controller/PomodoroController.java
@@ -0,0 +1,14 @@
+package org.example.Controller;
+
+import org.example.Model.PomodoroModel;
+import org.example.View.PomodoroView;
+
+public class PomodoroController {
+    PomodoroModel pomodoroModel = new PomodoroModel();
+    PomodoroView pomodoroView = new PomodoroView();
+
+    public PomodoroController(PomodoroModel pomodoroModel, PomodoroView pomodoroView) {
+       
+    }
+    
+}
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+        PomodoroTimerApp classUnderTest = new PomodoroTimerApp();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
